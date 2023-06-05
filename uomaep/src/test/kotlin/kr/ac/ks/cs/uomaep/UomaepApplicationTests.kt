b0f24231f84682ba21f339959731c25bdb1452ad
uomaep <uomaep@gmail.com>
2023-06-06 02:06:43 +0900

    spring boot

---
+++ uomaep/src/test/kotlin/kr/ac/ks/cs/uomaep/UomaepApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.ks.cs.uomaep
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UomaepApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
